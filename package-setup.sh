84d39bdf9961e783dcb6411a7c11d7835b03f913
timconradinc <tim@timconrad.org>
2013-11-06 18:11:46 -0500

    Update package-setup.sh

--- package-setup.sh
+++ package-setup.sh
@@ -5,6 +5,7 @@ sudo apt-get install python-software-properties lib32gcc1 libc6-dev-i386 python-
 sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-add-repository ppa:rwky/redis
 sudo apt-add-repository ppa:webupd8team/java
+sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
-sudo apt-get install ruby1.9.1 ruby1.9.1-dev rake rubygems ruby-switch
+sudo apt-get install ruby1.9.1 ruby1.9.1-dev rake rubygems ruby-switch fish
 git clone https://github.com/timconradinc/dotfiles.git
