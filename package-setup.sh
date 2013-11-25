681cb5ce7113003d8e272338956c6c6968e5109c
timconradinc <tim@timconrad.org>
2013-11-25 15:56:44 -0500

    Update package-setup.sh

--- package-setup.sh
+++ package-setup.sh
@@ -9,6 +9,7 @@ sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-add-repository ppa:rwky/redis
 sudo apt-add-repository ppa:webupd8team/java
 sudo apt-add-repository ppa:fish-shell/release-2
+sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
 
 sudo apt-get install python-software-properties lib32gcc1 libc6-dev-i386 python-pip python-setuptools python-dev screen vim bzr subversion build-essential unrar rcs exuberant-ctags perltidy pyflakes curl iotop atop
