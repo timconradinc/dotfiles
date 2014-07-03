c243202f031d6cf36f97df8bfeb9b90fb6a194cc
timconradinc <tim@timconrad.org>
2014-07-03 11:19:11 -0400

    Update package-setup.sh

--- package-setup.sh
+++ package-setup.sh
@@ -2,8 +2,8 @@
 
 mkdir -p ${HOME}/{bin,lib,workspace,tmp}
 # https://wiki.postgresql.org/wiki/Apt
-sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list.d/pgsql.list
-sudo wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+#sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list.d/pgsql.list
+#sudo wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 
 sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-add-repository ppa:rwky/redis
@@ -13,5 +13,5 @@ sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
 
 sudo apt-get install python-software-properties lib32gcc1 libc6-dev-i386 python-pip python-setuptools python-dev screen vim bzr subversion build-essential unrar rcs exuberant-ctags perltidy pyflakes curl iotop atop
-sudo apt-get install ruby1.9.1 ruby1.9.1-dev rake rubygems ruby-switch fish
+sudo apt-get install ruby1.9.1 ruby1.9.1-dev rake fish
 git clone https://github.com/timconradinc/dotfiles.git
